829dc7e536fbbbfaf930d488e7d3cba835fdb759
ejcoalejandro-create <ejcoalejandro@gmail.com>
2026-08-12 14:40:55 +0200

    superbase

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBh
 const supabasconnection = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 console.log('Supabase conectado:', supabaseconnection);
 
-async function cargarTareas() {
+async function getTasks() {
 try {
 const { data, error } = await supabase
 .from('tasks')
